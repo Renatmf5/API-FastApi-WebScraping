6053e92a750f6e61647678e6eeeb7e7ba4f8b674
renatmf5 <renato_mf5@hotmail.com>
2024-10-14 15:52:00 -0300

    prepara para deploy

--- core/config.py
+++ core/config.py
@@ -5,21 +5,21 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "Web Scraping API"
     # Security
-    JWT_SECRET: str = 'SYbwx85IxCd5TAA5vmopAR3_jKx9AbErgiIxIWZab8A'
+    JWT_SECRET: str
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7 # 60 minutos * 24 horas * 7 dias => 1 semana
     # Database
-    DATABASE_URL: str = "sqlite:///./authDB.db"
+    DATABASE_URL: str
     # Web Scraping
     URL_DOWNLOAD: str = "http://vitibrasil.cnpuv.embrapa.br/download"
     # Logging
     LOG_LEVEL: str = "info"
     # DataLake Bucket name
-    BUCKET_NAME: str = 'datalake-bucket-techchallenge1'
-    
-    
+    BUCKET_NAME: str
+    # Environment
+    ENV: str = "development"
+
+    class Config:
+        env_file = ".env"
+
 settings = Settings()
-
-
-#  "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJSZW5hdG9tIiwiZXhwIjoxNzI2NTk3Nzk1fQ.wB_Ad0KDciQSx72b0OOy5widRle_uEEuKj5wC4gvJjQ",
-#  "token_type": "bearer"
--- main.py
+++ main.py
@@ -6,23 +6,28 @@ from sqlmodel import SQLModel
 from core.database import engine
 from models.usuario_model import UsuarioModel  # Importe todos os modelos que você deseja criar
 
-app = FastAPI(title=settings.PROJECT_NAME)
-app.include_router(api_router, prefix=settings.API_V1_STR)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
+def get_application() -> FastAPI:
+    app = FastAPI(title=settings.PROJECT_NAME)
+    app.include_router(api_router, prefix=settings.API_V1_STR)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    @app.on_event("startup")
+    def on_startup():
+        create_db_and_tables()
+
+    return app
+
+app = get_application()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level=settings.LOG_LEVEL, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level=settings.LOG_LEVEL, reload=settings.ENV == "development")
