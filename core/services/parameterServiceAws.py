703392ab8e29ea2030691422f5c7eabdce0da752
renatmf5 <renato_mf5@hotmail.com>
2024-10-16 10:28:16 -0300

    ajuste application_start.sh

--- core/services/parameterServiceAws.py
+++ core/services/parameterServiceAws.py
@@ -3,7 +3,7 @@ import boto3
 
 # Função para obter parâmetros do SSM Parameter Store
 def get_ssm_parameter(name: str) -> str:
-    ssm_client = boto3.client('ssm')
+    ssm_client = boto3.client('ssm', region_name='us-east-1')
     try:
         response = ssm_client.get_parameter(Name=name, WithDecryption=True)
         return response['Parameter']['Value']
